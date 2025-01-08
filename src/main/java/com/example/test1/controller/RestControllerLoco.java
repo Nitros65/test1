d2552e70ef7c45c7c94fc481b6b77620e0868b37
Nitros64 <nitros.n64@gmail.com>
2025-01-08 03:03:28 +0100

    Service 2 listo

--- src/main/java/com/example/test1/controller/RestControllerLoco.java
+++ src/main/java/com/example/test1/controller/RestControllerLoco.java
@@ -1,6 +1,7 @@
 package com.example.test1.controller;
 
 import com.example.test1.service.ServiceOne;
+import com.example.test1.service.ServiceTwo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestControllerLoco {
 
     private final ServiceOne serviceOne;
+    private final ServiceTwo serviceTwo;
 
-    public RestControllerLoco(ServiceOne serviceOne) {
+    public RestControllerLoco(ServiceOne serviceOne, ServiceTwo serviceTwo) {
         this.serviceOne = serviceOne;
+        this.serviceTwo = serviceTwo;
     }
 
     @GetMapping
     public String getTask1() {
         return serviceOne.getMessageTask1();
+    }
+
+    @GetMapping("/service2")
+    public String getService2() {
+        return serviceTwo.getServiceTwo();
     }
 }
