55759ba6985f549af9ae1e24bee928f6df051bf7
Nitros64 <nitros.n64@gmail.com>
2025-01-08 03:25:17 +0100

    Service 3 listo

---
+++ src/main/java/com/example/test1/service/ServiceThree.java
@@ -0,0 +1,10 @@
+package com.example.test1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ServiceThree {
+    public String getServiceThree() {
+        return "Servicio 3";
+    }
+}
